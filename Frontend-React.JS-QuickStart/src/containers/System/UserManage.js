8c1c3bc081e254a260b6e7a6e0b18f28d61002cb
Loc <anhlocok123sa1@gmail.com>
2025-06-30 18:26:20 +0700

    #38 CRUD với React 2 - Hiển Thị Dữ Liệu Người Dùng Ra View (READ) | React Cho Người Mới Bắt Đầu

--- Frontend-React.JS-QuickStart/src/containers/System/UserManage.js
+++ Frontend-React.JS-QuickStart/src/containers/System/UserManage.js
@@ -2,20 +2,76 @@ import React, { Component } from 'react';
 // eslint-disable-next-line
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService'
 class UserManage extends Component {
+
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            arrUsers: []
+        }
+    }
 
     state = {
 
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
 
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="users-container">
+                <div className="title text-center">Manage user with React</div>
+                <div className="users-table">
+                    <table className="table table-bordered table-striped">
+                        <thead>
+                            <tr className='table-primary'>
+                                <th scope="col">#</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">First name</th>
+                                <th scope="col">Last name</th>
+                                <th scope="col">Address</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {arrUsers && arrUsers.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <th scope="row">{index}</th>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button type="button" className="btn-edit">
+                                                <i className="fas fa-pencil-alt"></i>
+                                            </button>
+                                            <button type="button" className="btn-delete">
+                                                <i className="fas fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
 
