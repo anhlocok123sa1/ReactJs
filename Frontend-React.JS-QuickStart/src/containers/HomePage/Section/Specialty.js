f0987453759a29ac6dd22ff626526afa708bc533
Loc <anhlocok123sa1@gmail.com>
2025-07-07 18:59:17 +0700

    #49 Customize Tùy Chỉnh React-Slider-Carousel Trong React App | React.JS Cho Người Mới Bắt Đầu

--- Frontend-React.JS-QuickStart/src/containers/HomePage/Section/Specialty.js
+++ Frontend-React.JS-QuickStart/src/containers/HomePage/Section/Specialty.js
@@ -7,8 +7,10 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import specialtyImg from '../../../assets/specialty/iStock-1366650119.jpg'
 
 class Specialty extends Component {
+
 
     render() {
 
@@ -22,27 +24,47 @@ class Specialty extends Component {
 
         return (
             <div className="section-specialty">
-                <div className="special-content">
-                    <Slider {...settings}>
-                        <div className='img-customize'>
-                            <h3>1</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>2</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>3</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>4</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>5</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>6</h3>
-                        </div>
-                    </Slider>
+                <div className="specialty-container">
+                    <div className="specialty-header">
+                        <span className='title-section'>Chuyên khoa phổ biến</span>
+                        <button className='btn-section'>Xem thêm</button>
+                    </div>
+                    <div className="specialty-body">
+                        <Slider {...settings}>
+                            <div className='img-customize'>
+                                <img src={specialtyImg} alt="specialtyImg" />
+                                <span>Cơ xương khớp 1</span>
+                            </div>
+                            <div className='img-customize'>
+                                <img src={specialtyImg} alt="specialtyImg" />
+                                <span>Cơ xương khớp 2</span>
+                            </div>
+                            <div className='img-customize'>
+                                <img src={specialtyImg} alt="specialtyImg" />
+                                <span>Cơ xương khớp 3</span>
+                            </div>
+                            <div className='img-customize'>
+                                <img src={specialtyImg} alt="specialtyImg" />
+                                <span>Cơ xương khớp 4</span>
+                            </div>
+                            <div className='img-customize'>
+                                <img src={specialtyImg} alt="specialtyImg" />
+                                <span>Cơ xương khớp 5</span>
+                            </div>
+                            <div className='img-customize'>
+                                <img src={specialtyImg} alt="specialtyImg" />
+                                <span>Cơ xương khớp 6</span>
+                            </div>
+                            <div className='img-customize'>
+                                <img src={specialtyImg} alt="specialtyImg" />
+                                <span>Cơ xương khớp 7</span>
+                            </div>
+                            <div className='img-customize'>
+                                <img src={specialtyImg} alt="specialtyImg" />
+                                <span>Cơ xương khớp 8</span>
+                            </div>
+                        </Slider>
+                    </div>
                 </div>
             </div>
         );
