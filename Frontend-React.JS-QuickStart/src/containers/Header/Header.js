b3e5cb1b172aaa2394eafa4ae694ae111bc9c004
Loc <anhlocok123sa1@gmail.com>
2025-07-10 20:40:30 +0700

    #55 Hoàn Thiện Menu Phân Quyền Dành Cho Admin | Redux-React Cho Người Mới Bắt Đầu

--- Frontend-React.JS-QuickStart/src/containers/Header/Header.js
+++ Frontend-React.JS-QuickStart/src/containers/Header/Header.js
@@ -6,6 +6,7 @@ import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from '../../utils'
+import { FormattedMessage } from 'react-intl';
 
 
 class Header extends Component {
@@ -15,7 +16,8 @@ class Header extends Component {
     }
 
     render() {
-        const { processLogout, language } = this.props;
+        const { processLogout, language, userInfo } = this.props;
+        console.log('check user info: ', userInfo);
 
         return (
             <div className="header-container">
@@ -25,6 +27,10 @@ class Header extends Component {
                 </div>
 
                 <div className="languages">
+                    <span className='welcome'>
+                        <FormattedMessage id="home-header.welcome" />
+                        , {userInfo && userInfo.firstName ? userInfo.firstName : ''}!
+                    </span>
                     <span
                         className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}
                         onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
@@ -52,7 +58,8 @@ class Header extends Component {
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
-        language: state.app.language
+        language: state.app.language,
+        userInfo: state.user.userInfo
     };
 };
 
